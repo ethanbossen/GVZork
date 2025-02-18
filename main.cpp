0ac0c5a03b71ec5a611d45f87d944745c07924a0
Ethan Bossenbroek <ethan.bossenbroek@gmail.com>
2025-02-18 11:10:23 -0500

    notes on game

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// At a metal festival, have to build the ultimate guitar, EVH bridge pickup from Hell
+// James Hetfield breaks his arm and you have to replace him, but in order to be the best
+// guitarist of all time you need to put together the *COOL_GUITAR_NAME* to play the biggest show
+// of all time, MOSCCOW 2045
+
+// Locations: 3 Floyds Beer Tent, Main Backstage (where you meet James), portapotty to hell (break up sections?)
+// side stage1 backstage, side stage2 backstage, side stage2 backstage, Founders Beer Tent, Mysterious camper's tent
+
+// NPCs: Lars, Kirk, EVH, Ozzy, James, Dave Mustaine, Lemmy, Dime & Vinnie, Jason Newsted
+
+// Items: EVH bridge pickup from hell, Blacktooth Grin, Dimebag's Neck, Tony Iommi's strings,
+// Jimmy Page's Neck Pickup, Jerry Cantrell's pickguard, Chuck Schuldiner's Pick, Kurt Cobain's Strings
+// Founder's beers (2), 3 floyds beers (2)
+
+class Item () {} // 10 items
+
+class Location () {}  // 8 locations
+
+class NPC () {} // 5 NPCs
+
+class Game () {} // 1 game
