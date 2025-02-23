b0144e974caa3f4369d0251aa8e3dce2305002d5
AsaRowntree <144493057+AsaRowntree@users.noreply.github.com>
2025-02-23 15:00:39 -0800

    trying to set up Game class. Having issues with setup_commands()

--- gvzork.h
+++ gvzork.h
@@ -26,39 +26,12 @@ private:
     float weight;
 
 public:
-
-    Item(const std::string& name, const std::string& description, int calories, float weight) {
-        if (name.empty()) {
-            throw std::invalid_argument("Name cannot be blank.");
-        }
-        if (description.empty()) {
-            throw std::invalid_argument("Description cannot be blank.");
-        }
-        if (calories < 0 || calories > 1000) {
-            throw std::invalid_argument("Calories must be between 0 and 1000.");
-        }
-        if (weight < 0 || weight > 500) {
-            throw std::invalid_argument("Weight must be between 0 and 500.");
-        }
-
-        this->name = name;
-        this->description = description;
-        this->calories = calories;
-        this->weight = weight;
-    }
-    
-    std::string getName() const { return name; }
-    
-    std::string getDescription() const { return description; }
-    
-    int getCalories() const { return calories; }
-    
-    float getWeight() const { return weight; }
-
-    friend std::ostream& operator<<(std::ostream& os, const Item& item) {
-        os << item.name << " (" << item.calories << " calories)- " << item.weight << " lb- " << item.description;
-        return os;
-    }
+    Item(const std::string& name, const std::string& description, int calories, float weight);
+    std::string getName() const;
+    std::string getDescription() const;
+    int getCalories() const;
+    float getWeight() const;
+    friend std::ostream& operator<<(std::ostream& os,const Item& item);
 };
 
 
@@ -70,36 +43,12 @@ private:
     int messageNumber;
 
 public:
-    NPC(const std::string& name, const std::string& description) {
-        if (name.empty() || description.empty()) {
-            throw std::invalid_argument("Name and description cannot be blank.");
-        }
-
-        this->name = name;
-        this->description = description;
-    }
-    
-    std::string getName() const { return name; }
-    
-    std::string getDescription() const { return description; }
-
-    void addMessage(const std::string& message) {
-        messages.push_back(message);
-    }
-
-    std::string getMessage() {
-        if (messages.empty()) {
-            return ("This NPC has no messages.");
-        }
-        std::string message = messages[messageNumber];
-        messageNumber = (messageNumber + 1) % messages.size();
-        return message;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const NPC& npc) {
-        os << npc.name;
-        return os;
-    }
+    NPC(const std::string& name, const std::string& description);
+    std::string getName() const;
+    std::string getDescription() const;
+    void addMessage(std::string& message);
+    std::string getMessage();
+    friend std::ostream& operator<<(std::ostream& os,const NPC& npc);
 
 };
 
@@ -113,90 +62,16 @@ private:
     bool visited;
 
 public:
-    Location(const std::string& name, const std::string& description) {
-        if (name.empty() || description.empty()) {
-            throw std::invalid_argument("Name and description cannot be blank.");
-        }
-
-        this->name = name;
-        this->description = description;
-
-    }
-
-    std::map<std::string, Location*> get_locations() const { return neighbors;}
-    
-    void add_location(std::string& direction, Location* location){
-        if (direction.empty()){
-            throw std::invalid_argument("Direction cannot be empty.");
-        }
-        if (neighbors.count(direction) > 0){
-            throw std::invalid_argument("That direction is already mapped for this location.");
-        }
-        neighbors[direction] = location;
-    }
-
-    void add_npc(NPC npc){
-        npcs.push_back(npc);
-    }
-
-    std::vector<NPC> get_npcs() const { return npcs;}
-    
-    void add_item(Item item){
-        items.push_back(item);
-    }
-    
-    std::vector<Item> get_items() const { return items;}
-    
-    void set_visited(){
-        visited = true;
-    }
-
-    bool get_visited(){
-        if (this->visited) {
-            return true;
-        }
-        return false;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Location& location) {
-        // Location name and description
-        os << location.name << "- " << location.description << "\n\n";
-
-        // List NPCs
-        os << "You see the following NPCs:\n";
-        if (location.npcs.empty()) {
-            os << "- None\n";
-        } else {
-            for (const NPC& npc : location.npcs) {
-                os << "- " << npc << "\n";
-            }
-        }
-
-        // List items
-        os << "\nYou see the following Items:\n";
-        if (location.items.empty()) {
-            os << "- None\n";
-        } else {
-            for (const Item& item : location.items) {
-                os << "- " << item.getName() << " (" << item.getCalories() << " calories) - "
-                   << item.getWeight() << " lb- " << item.getDescription() << "\n";
-            }
-        }
-
-        // List directions
-        os << "\nYou can go in the following Directions:\n";
-        if (location.neighbors.empty()) {
-            os << "- None\n";
-        } else {
-            for (const auto& neighbor : location.neighbors) {
-                os << "- " << neighbor.first << "- "
-                   << (neighbor.second->get_visited() ? neighbor.second->name : "Unknown")
-                   << (neighbor.second->get_visited() ? " (Visited)" : "") << "\n";
-            }
-        }
-
-        return os;
-    }
+    Location(const std::string& name, const std::string& description);
+    std::map<std::string, Location*> get_locations() const;
+    void add_location(std::string& direction, Location* location);
+    void add_npc(NPC npc);
+    std::vector<NPC> get_npcs() const;
+    void add_item(Item& item);
+    std::vector<Item> get_items() const;
+    void set_visited();
+    bool get_visited() const;
+    friend std::ostream& operator<<(std::ostream& os, const Location& location);
 };
 
 class Game {
@@ -205,22 +80,30 @@ public:
     void play();
     void executeCommand(std::string command, std::vector<std::string> args);
     void normalizeCommand(std::string& command);
+    
 
 
 private:
-    void createWorld();
-    void showHelp(std::vector<std::string> args);
-    void talk(std::vector<std::string> args);
-    void move(std::vector<std::string> args);
-    void take(std::vector<std::string> args);
-    void drink(std::vector<std::string> args);
-    void look(std::vector<std::string> args);
-    void quit(std::vector<std::string> args);
-    Location* currentLocation;
-    std::vector<Location> locations;
+    std::map<std::string, std::function<void(*)(std::vector<std::string>)>> commands;
     std::vector<Item> inventory;
+    int currentWeight;
+    std::vector<Location> locations;
+    Location* currentLocation;
+    int caloriesNeeded;
+    bool inProgress;
     int drunkness;
-    std::map<std::string, std::function<void(std::vector<std::string>)>> commands;
+    void createWorld();
+    std::map<std::string, std::function<void(*)(std::vector<std::string>)>> setup_commands();
+    Location* randomLocation();
+    void showHelp();
+    void talk(std::vector<std::string> target);
+    void meet(std::vector<std::string> target);
+    void take(std::vector<std::string> target);
+    void give(std::vector<std::string> target);
+    void go(std::vector<std::string> target);
+    void showItems(std::vector<std::string> target);
+    void look(std::vector<std::string> target);
+    void quit(std::vector<std::string> target);
     std::map<std::string, std::string> commandAliases;
 };
 
